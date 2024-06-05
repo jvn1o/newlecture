d9ec4c0021e488aa77935e6d435372a6ad32070f
jvn1o <junok0987@naver.com>
2024-06-05 14:30:07 +0900

    mycode

---
+++ mycode/App.java
@@ -0,0 +1,24 @@
+class App{
+	public static void main(String[] args) {	
+
+		int kor1; // int는 정수형
+		int kor2;
+		int kor3;
+		int total;
+		float avg; // float은 실수형
+
+		kor1 = 90;
+		kor2 = 60;
+		kor3 = 30;
+
+		total = kor1 + kor2 + kor3;
+		avg = total / 3;
+
+		System.out.printf("kor1 = %d\n", kor1);	
+		System.out.printf("kor2 = %d\n", kor2);	
+		System.out.printf("kor3 = %d\n", kor3);
+		System.out.printf("total = %d\n", total);
+		System.out.printf("avg = %f\n", avg);
+
+		}
+	}
