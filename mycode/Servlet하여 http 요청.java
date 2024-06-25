ec61f9e82bd5c9b73ecc3bff44c59f3f155cdba1
jvn1o <junok0987@naver.com>
2024-06-25 18:27:37 +0900

    240625

---
+++ mycode/Servlet하여 http 요청.java
@@ -0,0 +1,15 @@
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import java.io.*;
+import jakarta.servlet.annotation.WebServlet;
+
+// @WebServlet("/hi")
+public class FirstServlet extends HttpServlet {
+    public void service(HttpServletRequest request,
+                        HttpServletResponse response)
+                        throws IOException, ServletException {
+        System.out.println("hello Servlet");
+        PrintWriter out = response.getWriter();
+        out.println("Hello Servlet!!!");
+    }
+}
